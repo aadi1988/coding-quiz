5fa053cc55fca6e3c46efa158cf349b1c730d61d
Aaditi <aadi2007.aaditi@gmail.com>
2020-08-04 23:04:32 -0700

    change document elements on clicking start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,10 @@ var btnClick = document.querySelector("#btn");
 
 var seconds = 60;
 
+var newContent = document.querySelector("#content");
+
+var newSubContent = document.querySelector("#subcontent");
+
 var timeFunc = function(){
      if(seconds === 0){
          clearInterval(stopTimer);
@@ -22,5 +26,24 @@ var timeFunc = function(){
 var stopTimer = function(){
    
     setInterval(timeFunc,1000);
+    
+    newContent.firstElementChild.textContent = "What's your name";
+    newSubContent.removeChild(newSubContent.childNodes[1]);
+    console.log(newSubContent.childNodes);
+    var ulEl = document.createElement("ul");
+    
+    for(var i = 0; i<4; i++){
+        var liEl = document.createElement("li");
+        liEl.className = "listEl"
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "btn";
+        var text = document.createTextNode(i+1 + ". Javascript");
+        buttonEl.appendChild(text);
+        liEl.appendChild(buttonEl);
+        ulEl.appendChild(liEl);
+    }
+    
+    
+    newSubContent.appendChild(ulEl);
 }
 var changeTimerEl = btnClick.addEventListener("click",stopTimer);
